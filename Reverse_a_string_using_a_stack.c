853a1f1c6b198bc3f9cbd1e67393e11bf774fcd0
LilAn09 <1327161394@qq.com>
2025-12-05 18:51:48 +0800

    用栈实现字符串逆序（添加注释）

--- Reverse_a_string_using_a_stack.c
+++ Reverse_a_string_using_a_stack.c
@@ -7,16 +7,21 @@ typedef struct
     DataType data[MAX_SIZE];
     int top;
 }SqStack;
-
+//函数功能 初始化栈
+//SqStack *s是一个指向结构体SqStack的指针
 void InitStack(SqStack *s){
     s->top=-1;
 }
+//函数功能 判栈空
 int StackEmpty(SqStack *s){
     return s->top==-1;
 }
+//函数功能 判栈满
 int StackFull(SqStack *s){
     return s->top==MAX_SIZE-1;
 }
+//进栈操作
+//x为当前读入的数据
 void Push(SqStack *s,DataType x){
     if(StackFull(s)){
         printf("overflow.");
@@ -25,6 +30,8 @@ void Push(SqStack *s,DataType x){
     s->top++;
     s->data[s->top]=x;
 }
+//出栈操作
+//*x是指向存储最先出栈的变量的指针
 int Pop(SqStack *s,DataType *x){
     if(StackEmpty(s)){
         return 0;
@@ -42,7 +49,7 @@ int main(){
         x=getchar();
         if(x!='\n'){
             Push(&stack,x);
-        }
+        }//注意：getchar是不会自己读入'\0'的
     }while (x!='\n');
 
     char ch;
